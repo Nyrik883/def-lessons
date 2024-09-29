8aa620c4b086cff82595cd6bab7a59075c8f6dc2
Nyrik883 <amogus.abobusov882@gmail.com>
2024-09-29 12:41:57 +0300

    programming lessons
    :)

---
+++ УРОКИ ПРГ/main (1).py
@@ -0,0 +1,49 @@
+#  list1 = [1, 4, "hello", True , [1, 2, ['win', 1]]]
+# x1 = [4][2][0]
+# print(x1)
+
+# dict1 = {}
+
+# phonebook = {"mom": "02323014412", "dad": "12349112812"}
+# print(phonebook["mom"])
+
+# phonebook["mom"] = "02323014412"
+# phonebook["friend"] = "1234"
+
+# del phonebook["friend"]
+
+# list1 = [1,2]
+# del list1[0]
+# print(list1)
+
+# print(phonebook)
+
+
+# set1 = {1, 1, 1, 2, 2, 3, 3,}
+# l1 = [1, 1, 1, 2, 2, 3, 3 ]
+# print(list(set(l1)))
+
+# while / for 
+
+# prods = ['apple' , 'banana' , 'milk']
+
+# for ip in prods:
+#     # if i == "milk":
+#     #     continue
+    
+#     for char in ip:
+#         print(char)
+    
+#     print(ip)
+
+# x = 10
+# while x > 10:
+#     print(x)
+#     x = x - 1
+
+x = None 
+while x != '12345':
+    x = input("::")
+
+
+
---
+++ УРОКИ ПРГ/main.py
@@ -0,0 +1,122 @@
+# g = 2
+# num1 = 45
+# num2 = 23
+# num3 = 101
+# result = num1 + num2 - 11 - num3 * 4
+# print(result)
+
+# fname = "jhon"
+# lname = "smit"
+
+# res = fname + " " + lname
+# res1 = fname * 5
+# it = 80
+
+# print(f"hello {fname} {5 + 5}")
+
+# my_b = True
+# my_b1 = False
+
+# b1 = 2 > 3
+# b2 = 2 < 3 
+# b3 = 2 == 3
+# b4 = 2 != 3
+# b5 = 2 >= 3
+# b6 = 2 <= 3
+# print(b5)
+
+# user_name = input("enter your name: ")
+# print(user_name)
+
+
+
+# user_name = input("Enter your name: ")
+ 
+# if user_name == "D":
+#     print("доступ получен")
+# elif user_name == "A":
+#     print("доступ получен")
+# elif user_name == "S":
+#     print("доступ получен")
+# else:
+#     print("НЕЛЬЗЯ")
+    
+# X1 = 10
+
+# if x1 == 10:
+#     print("+")
+# if x1 == 9: 
+#     print("+")
+# if x1 == 8:
+#     print("+")
+# if x1 == 7:
+#     print("+")
+# if x1 == 6:
+#     print("+")
+
+
+
+
+# q1 = input("сколько метров в одной миле? Ваш ответ: ")
+# q2 = input("столица канады? Ваш ответ: ")
+# q3 = input("сколько существует законов ньютона? Ваш ответ: ")
+
+# res = 0 
+
+# if int(q1) == 100:
+#     res = res + 1
+# else:
+#     res = res + 0
+# if int(q2) == 0:
+#     res = res + 1
+# else:
+#     res = res + 0
+# if int(q3) == 3:
+#     res = res + 1
+# else:
+#     res = res + 0
+    
+# print (f" Вы ответили на  {res} вопросов")
+
+# "5" -> 5 (int)
+# "5" -> "5" (str)
+
+
+
+
+
+
+# НОВЫЙ УРОК _______________________________________________________________
+
+# def sum( a , b , c):
+#     print(a + b + c)
+
+#     return a + b + c
+
+# result = sum(1,2,3) + 10
+# print(result)
+
+
+
+# calc(10 ,20 , "-") -> -10
+# calc(30 ,2 , "*") -> 60
+
+
+# def calc( a ,b ,c ):
+#     if c == "-":
+#         return a - b
+
+#     if c == "+":
+#         return a + b  
+
+# print(calc(1,2,"+"))
+# print(calc(1,2,"-"))
+
+
+def x(a , b):
+    print(a + b)
+    return a + b
+
+x(2 , 2)
+
+
